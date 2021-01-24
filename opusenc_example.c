ff20d22f6e03ed2cabbe771552ed4dad729b2539
Gábor Zóka <59315013+gabor-zoka@users.noreply.github.com>
2021-01-24 18:52:32 +0000

    Changed to read from a float buffer. We will use the float to scale the volume of the audio.

--- opusenc_example.c
+++ opusenc_example.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "opusenc.h"
+#include <stdlib.h>
+#include <opusenc.h>
 
 #define READ_SIZE 256
 
@@ -27,16 +28,28 @@ int main(int argc, char **argv) {
     fclose(fin);
     return 1;
   }
+
+  float* data = malloc(2*READ_SIZE*sizeof(float));
+  if (!data) {
+    fprintf(stderr, "Memory allocation error");
+    return 1;
+  }
+
   while (1) {
     short buf[2*READ_SIZE];
     int ret = fread(buf, 2*sizeof(short), READ_SIZE, fin);
     if (ret > 0) {
-      ope_encoder_write(enc, buf, ret);
+      short* fend=buf+2*ret;
+      float* t=data;
+      for (short* f=buf; f!=fend; ++f,++t)
+        *t=(float)*f/32768.f;
+      ope_encoder_write_float(enc, data, ret);
     } else break;
   }
   ope_encoder_drain(enc);
   ope_encoder_destroy(enc);
   ope_comments_destroy(comments);
   fclose(fin);
+  free(data);
   return 0;
 }
