9a66d34865fa1e609ee39cb122ab6dd5b4922673
Gábor Zóka <59315013+gabor-zoka@users.noreply.github.com>
2021-01-30 04:09:06 +0000

    Updated the readme.sh

--- readme.sh
+++ readme.sh
@@ -1,10 +1,3 @@
-gcc opusenc_example.c -lopusenc -I/usr/include/opus
+gcc flac_decode_example.c -lFLAC -lopusenc -lm -I/usr/include/opus
 
-flac --force-raw-format --endian little --sign signed -wfd TomsDiner.flac
-
-./a.out TomsDiner.raw TomsDiner.opus
-
-# The flac decoder:
-
-gcc flac_decode_example.c -lFLAC
 ./a.out TomsDiner.flac TomsDiner.wav
